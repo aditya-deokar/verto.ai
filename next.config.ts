0ac86fc27650f7a2980bfa9d42176fac98205cad
adiya-deokar <adityadeokar80@gmail.com>
2025-12-04 00:37:24 +0530

    chore: upgrade Next.js to v16, React to v19, and add React Compiler plugin.

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  reactCompiler: true,
   /* config options here */
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol:'https',
-        hostname:'images.unsplash.com',
-        port:'',
-        pathname:'/**',
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol:'https',
-        hostname:'plus.unsplash.com',
-        port:'',
-        pathname:'/**',
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol:'https',
-        hostname:'via.placeholder.com',
-        port:'',
-        pathname:'/**',
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol:'https',
-        hostname:'uncarecdn.com',
-        port:'',
-        pathname:'/**',
+        protocol: 'https',
+        hostname: 'uncarecdn.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol:'https',
-        hostname:'oaidalleapiprodscus.blob.core.window.net',
-        port:'',
-        pathname:'/**',
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.window.net',
+        port: '',
+        pathname: '/**',
       },
     ]
   }
